39a653c319808279b0a2d7e0dd59d66d91ebc3f4
Hercul3an <91543278+Hercul3an@users.noreply.github.com>
2022-11-20 12:46:46 +0000

    Initial Contribution

--- src/main/java/me/artiya/jiradt4j/LetHeavenDoIt.java
+++ src/main/java/me/artiya/jiradt4j/LetHeavenDoIt.java
@@ -4,3 +4,8 @@ package me.artiya.jiradt4j;
 /**
  * Created by robot on 24/07/15.
  */
+public class LetHeavenDoIt {
+    public static void main(String[] args) {
+        // เรื่อง 'บางเรื่อง'
+        // ปล่อยให้ 'สวรรค์'
+        // ทำหน้าที่
