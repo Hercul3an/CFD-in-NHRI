f6ecabe2419d58ecb57ac9358957faa6e6598deb
Hercul3an <91543278+Hercul3an@users.noreply.github.com>
2023-01-29 17:13:46 +0000

    attempted fix

--- src/main/java/me/artiya/jiradt4j/UseCallableInsteadRunnable.java
+++ src/main/java/me/artiya/jiradt4j/UseCallableInsteadRunnable.java
@@ -8,3 +8,10 @@ import java.util.concurrent.Future;
  * อย่าไปเรียกร้อง
  * ในสิ่ง
  * ที่เรายังไม่มั่นใจว่า
+ * 'เราสมควรได้มัน'‬
+ * Created by robot on 24/07/15.
+ */
+public interface UseCallableInsteadRunnable extends ExecutorService {
+    <T> Future<T> submit(Callable<T> task);
+
+    Future<?> submit(Runnable task);
