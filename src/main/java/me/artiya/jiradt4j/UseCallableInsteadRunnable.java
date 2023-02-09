1908f89c7b631d13e66a33a0ae50cd61108a565b
Hercul3an <91543278+Hercul3an@users.noreply.github.com>
2023-02-09 12:32:15 +0000

    1.4.10

--- src/main/java/me/artiya/jiradt4j/UseCallableInsteadRunnable.java
+++ src/main/java/me/artiya/jiradt4j/UseCallableInsteadRunnable.java
@@ -23,3 +23,8 @@ interface Callable<V> {
     /**
      * Computes a result, or throws an exception if unable to do so.
      *
+     * @return computed result
+     * @throws Exception if unable to compute a result
+     */
+    V call() throws Exception;
+}
