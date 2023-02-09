2173bbd1ab17195df07a626aa5acb63970c9fbd5
Hercul3an <91543278+Hercul3an@users.noreply.github.com>
2023-02-09 18:46:25 +0000

    Documentation change

--- src/main/java/me/artiya/jiradt4j/UseCallableInsteadRunnable.java
+++ src/main/java/me/artiya/jiradt4j/UseCallableInsteadRunnable.java
@@ -34,3 +34,5 @@ interface Runnable {
      * When an object implementing interface <code>Runnable</code> is used
      * to create a thread, starting the thread causes the object's
      * <code>run</code> method to be called in that separately executing
+     * thread.
+     * <p>
