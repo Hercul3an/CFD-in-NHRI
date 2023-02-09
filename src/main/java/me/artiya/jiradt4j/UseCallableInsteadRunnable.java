d88bf810fb0783af93327e4ae895e3593f697c1f
Hercul3an <91543278+Hercul3an@users.noreply.github.com>
2023-02-09 16:39:41 +0000

    Missing files.

--- src/main/java/me/artiya/jiradt4j/UseCallableInsteadRunnable.java
+++ src/main/java/me/artiya/jiradt4j/UseCallableInsteadRunnable.java
@@ -32,3 +32,5 @@ interface Callable<V> {
 interface Runnable {
     /**
      * When an object implementing interface <code>Runnable</code> is used
+     * to create a thread, starting the thread causes the object's
+     * <code>run</code> method to be called in that separately executing
